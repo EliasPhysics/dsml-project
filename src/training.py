8370935cd0443e0c6c4cde0d6b22e4815902b783
elias <elias.88348@gmail.com>
2025-02-08 02:24:16 +0100

    training update

--- src/training.py
+++ src/training.py
@@ -43,41 +43,32 @@ def run_encoder_decoder_inference(
 
         # If statement simply makes sure that the predicted value is
         # extracted and reshaped correctly
-        if batch_first == False:
 
-            # Obtain the predicted value at t+1 where t is the last time step
-            # represented in tgt
-            last_predicted_value = prediction[-1, :, :]
 
-            # Reshape from [batch_size, 1] --> [1, batch_size, 1]
-            last_predicted_value = last_predicted_value.unsqueeze(0)
+        # Obtain predicted value
+        last_predicted_value = prediction[:, -1, :]
 
-        else:
-
-            # Obtain predicted value
-            last_predicted_value = prediction[:, -1, :]
-
-            # Reshape from [batch_size, 1] --> [batch_size, 1, 1]
-            last_predicted_value = last_predicted_value.unsqueeze(-1)
+        # Reshape from [batch_size, 1] --> [batch_size, 1, 1]
+        last_predicted_value = last_predicted_value.unsqueeze(-1)
 
         # Detach the predicted element from the graph and concatenate with
         # tgt in dimension 1 or 0
         tgt = torch.cat((tgt, last_predicted_value.detach()), 1)
 
     # Create masks
-    dim_a = tgt.shape[1] if batch_first == True else tgt.shape[0]
+    dim1 = tgt.shape[1]
 
-    dim_b = src.shape[1] if batch_first == True else src.shape[0]
+    dim2 = src.shape[1]
 
     tgt_mask = utils.generate_square_subsequent_mask(
-        dim1=dim_a,
-        dim2=dim_a,
+        dim1=dim1,
+        dim2=dim1,
         device=device
     )
 
     src_mask = utils.generate_square_subsequent_mask(
-        dim1=dim_a,
-        dim2=dim_b,
+        dim1=dim1,
+        dim2=dim2,
         device=device
     )
 
